160e4ffff6e292b21649a6f9f160091d43a08251
Amjad <amjad1503d@aptechgdn.net>
2026-04-08 12:41:30 +0500

    chore(deploy): prepare backend for Railway

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const configureRoutes = (app) => {
   app.use('/api/auth', require('./api/auth'));
   app.use('/api/users', require('./api/users'));
   app.use('/api/chips', require('./api/chips'));
+  app.get('/healthz', (req, res) => {
+    res.status(200).json({ ok: true });
+  });
   app.use('/', (req, res) => {
     res.status(200).send('RollPlay API Documents');
   });
